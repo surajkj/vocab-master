89c0f404c582b8c77b83676f2f3e83ac901770bf
suraj <srj121.kj@gmail.com>
2025-12-15 15:17:44 +0900

    Add build files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Vocab Master
-
---
+++ dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vocab-master/sw.js', { scope: '/vocab-master/' })})}
